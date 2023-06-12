b03a5be4b89e1432cd5b4203cd464d1e56fd67e6
Roger Edgar Romualdo Javier <roger@Rogers-MacBook-Pro.local>
2023-06-12 01:54:39 -0500

    s3-1

---
+++ S3_conecction.py
@@ -0,0 +1,7 @@
+import boto3
+print('Lista de buckets: ')
+s3resource = boto3.resource('s3','us-east-1')
+buckets = s3resource.buckets.all()
+for bucket in buckets:
+    print(f'\t{bucket.name}') 
+
